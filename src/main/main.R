16ebe58b79abdcad45ff2fcb62a0249ceee52efc
lrennels <lrennels@berkeley.edu>
2020-04-14 22:50:09 -0700

    Add visualization options

--- src/main/main.R
+++ src/main/main.R
@@ -14,7 +14,7 @@ source("src/main/viz_leaflet.R")
 # above this file and contains the `data` and `src` primary folders. All paths are relative to this
 # directory.
 
-# 2. Your options for species are: "Verdin", "Crissal_Thrasher", "Black-tailed-Gnatcatcher", and "Bells-Vireo"
+# 2. Your options for species are: "Verdin", "Crissal_Thrasher", "Black-tailed_Gnatcatcher", and "Bells_Vireo"
 
 # 3. If you want to visualize in your default RStudio viewer, no action is needed. If you would prefer to force
 # the visualization to appear in a local html browser, toggle this on with set_null_viewer() and off at the
@@ -45,23 +45,36 @@ scaling_factors <- read_csv("data/intermediate/scale_factors.csv")
 ## Example 1. Baseline Climate Conditions
 ##
 
+# constants
 species = "Verdin"
+
+# calculate raster and map it
 occu_surface <- predict_occu_surface(prism_stack, all_covs, species, scaling_factors)
-get_leaflet(occu_surface, "%")
+get_leaflet(occu_surface)
+
+# the following will do both functions above in one:
+get_leaflet_occu_surface(prism_stack, all_covs, species, scaling_factors, c(1, 0, 0, 0)) # baseline climate
 
 ##
 ## Example 2. Use New Climate Conditions
 ##
 
+# constants
 species = "Verdin"
 climate_factors = c(0.8, 1, 1, 1) # 80% of precip, increases of 1 deg C for all temperature variables
+
+# calculate raster and map it
 new_prism_stack = scale_input_stack(prism_stack, climate_factors)
 occu_surface_new <- predict_occu_surface(new_prism_stack, all_covs, species, scaling_factors)
-get_leaflet(occu_surface_new, "%")
+get_leaflet(occu_surface_new)
+
+# the following will do both functions above in one:
+get_leaflet_occu_surface(new_prism_stack, all_covs, species, scaling_factors, climate_factors)
 
 ##
-## Example 3. Use built in functions (eventually wrap this in RShiny)
+## Example 3. Map occupancy surfaces of all four species at once (note that for now it must be the four 
+## species listed above, but in the future this can be expanded to be flexible for subgroups or expanded
+## groups of species)
 ##
+get_leaflet_occu_surface_allspecies(prism_stack, all_covs, scaling_factors, c(1, 0, 0, 0)) # baseline climate
 
-species = "Crissal_Thrasher"
-get_leaflet_occu_surface(prism_stack, all_covs, species, scaling_factors, c(1, 0, 0, 0)) # baseline climate
--- src/main/viz_leaflet.R
+++ src/main/viz_leaflet.R
@@ -19,9 +19,9 @@ project_raster_to_WGS84 <- function(myraster) {
 }
 
 # get_leaflet: function to use leaflet to display a raster
-#   inputs: input raster and title
+#   inputs: input raster
 #   outputs: leaflet visualization
-get_leaflet <- function(myraster, mytitle) {
+get_leaflet <- function(myraster) {
   
   # get color palette
   pal <- colorNumeric(c("#FFFFCC", "#41B6C4", "#0C2C84"), values(myraster),
@@ -35,7 +35,7 @@ get_leaflet <- function(myraster, mytitle) {
     
     # map groups
     addRasterImage(myraster, colors = pal, opacity = 0.8, group = "Occupancy Surface") %>%
-    addLegend(pal = pal, values = values(myraster),title = mytitle, group = "Occupancy Surface") %>%
+    addLegend(pal = pal, values = values(myraster),title = "probability", group = "Occupancy Surface") %>%
   
     # marker groups
     # TODO - add points
@@ -45,9 +45,57 @@ get_leaflet <- function(myraster, mytitle) {
       baseGroups = c("State Lines (default)"),
       overlayGroups = c("Occupancy Surface"),
       options = layersControlOptions(collapsed = FALSE)
-     ) %>% 
+     )
     
-    hideGroup("Occupancy Surface") # start with occupancy surface hidden
+  return (map)
+}
+
+# get_leaflet_allspecies: function to use leaflet to display all four occupancy surface rasters, note that as of now
+# this function hard codes the four species but can be expanded to deal with a varying number of species and
+# species names
+#   inputs: input raster stack
+#   outputs: leaflet visualization
+get_leaflet_allspecies <- function(rasters) {
+  
+  # Species are assumed to be as follows and in order:
+  #
+  # 1. "Verdin"
+  # 2. "Crissal_Thrasher"
+  # 3. "Black-tailed_Gnatcatcher"
+  # 4. "Bells_Vireo"
+
+  
+  # get color palette
+  vals = c(values(rasters[[1]]), values(rasters[[2]]), values(rasters[[3]]), values(rasters[[4]]))
+  pal <- colorNumeric(c("#FFFFCC", "#41B6C4", "#0C2C84"), NULL,
+                      na.color = "transparent")
+  
+  # use leaflet to display the raster (https://rstudio.github.io/leaflet/raster.html)
+  map <- leaflet() %>% 
+    
+   
+    # base layers
+    addTiles(group = "Base") %>%
+    addLegend(pal = pal, values = vals,title = "probability", group = "Base") %>%
+    
+    # overlay layers -markers and maps
+    
+    # TODO - add points which will have group "Observation Points"
+    
+    addRasterImage(rasters[[1]], colors = pal, opacity = 0.8, group = "Verdin") %>%
+    addRasterImage(rasters[[2]], colors = pal, opacity = 0.8, group = "Crissal_Thrasher") %>%
+    addRasterImage(rasters[[3]], colors = pal, opacity = 0.8, group = "Black-tailed_Gnatcatcher") %>%
+    addRasterImage(rasters[[4]], colors = pal, opacity = 0.8, group = "Bells_Vireo") %>%
+    
+  
+    # layer control (https://rstudio.github.io/leaflet/showhide.html)
+    addLayersControl(
+      baseGroups = c("Verdin", "Crissal_Thrasher", "Black-tailed_Gnatcatcher", "Bells_Vireo"),
+      overlayGroups = c("Observation Points"),
+      options = layersControlOptions(collapsed = FALSE)
+    ) %>% 
+    
+    hideGroup("Observation Points") # start with points hidden
   
   return (map)
 }
@@ -60,9 +108,34 @@ get_leaflet_occu_surface <- function(input_stack, all_covs, species, scaling_fac
   
   input_stack_scaled = scale_input_stack(input_stack, climate_factors)
   occu_surface <- predict_occu_surface(input_stack_scaled, all_covs, species ,scaling_factors)
-  map <- get_leaflet(occu_surface, "%")
+  map <- get_leaflet(occu_surface)
   
   return(map)
   
+}
+
+# get_leaflet_occu_surface_allspecies: function to display all four occupancy surfaces, note that as of now
+# this function hard codes the four species but can be expanded to deal with a varying number of species and
+# species names
+#   inputs: climate variables, covariates, scaling factors, and climate factors (to scale climate)
+#   outputs: leaflet visualization
+
+get_leaflet_occu_surface_allspecies <- function(input_stack, all_covs, scaling_factors, climate_factors){
+  
+  input_stack_scaled = scale_input_stack(input_stack, climate_factors)
+  
+  occu_surface_thrasher     <- predict_occu_surface(prism_stack, all_covs, "Crissal_Thrasher", scaling_factors)
+  occu_surface_verdin       <- predict_occu_surface(prism_stack, all_covs, "Verdin", scaling_factors)
+  occu_surface_gnatcatcher  <-predict_occu_surface(prism_stack, all_covs, "Black-tailed_Gnatcatcher", scaling_factors)
+  occu_surface_vireo        <- predict_occu_surface(prism_stack, all_covs, "Bells_Vireo", scaling_factors)
+  
+  occu_surface_stack <- stack(occu_surface_thrasher,
+                              occu_surface_verdin,
+                              occu_surface_gnatcatcher,
+                              occu_surface_vireo)
+  
+  map <- get_leaflet_allspecies(occu_surface_stack)
+  
+  return(map)
 }
 
